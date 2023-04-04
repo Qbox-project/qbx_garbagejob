b66323abe77026e021508b3528695274a70f6668
ChatDisabled <44729807+ChatDisabled@users.noreply.github.com>
2023-04-04 22:58:29 +0200

    fix: export naming

--- client/main.lua
+++ client/main.lua
@@ -1,4 +1,4 @@
-local QBCore = exports['qb-core']:GetCoreObject()
+local QBCore = exports['qbx-core']:GetCoreObject()
 local playerJob = nil
 local garbageVehicle = nil
 local hasBag = false
@@ -208,17 +208,17 @@ local function RunWorkLoop()
                     if Distance < 1.5 then
                         if not GarbText then
                             GarbText = true
-                            exports['qb-core']:DrawText(Lang:t("info.grab_garbage"), 'left')
+                            exports['qbx-core']:DrawText(Lang:t("info.grab_garbage"), 'left')
                         end
                         if IsControlJustPressed(0, 51) then
                             hasBag = true
-                            exports['qb-core']:HideText()
+                            exports['qbx-core']:HideText()
                             TakeAnim()
                         end
                     elseif Distance < 10 then
                         if GarbText then
                             GarbText = false
-                            exports['qb-core']:HideText()
+                            exports['qbx-core']:HideText()
                         end
                     end
                 else
@@ -230,7 +230,7 @@ local function RunWorkLoop()
                         if TruckDist < 2 then
                             if not TrucText then
                                 TrucText = true
-                                exports['qb-core']:DrawText(Lang:t("info.dispose_garbage"), 'left')
+                                exports['qbx-core']:DrawText(Lang:t("info.dispose_garbage"), 'left')
                             end
                             if IsControlJustPressed(0, 51) and hasBag then
                                 StopAnimTask(PlayerPedId(), 'missfbi4prepp1', '_bag_walk_garbage_man', 1.0)
@@ -289,7 +289,7 @@ local function RunWorkLoop()
 
                                         Wait(1500)
                                         if TrucText then
-                                            exports['qb-core']:HideText()
+                                            exports['qbx-core']:HideText()
                                             TrucText = false
                                         end
                                     end, function() -- Cancel
@@ -325,7 +325,7 @@ local function CreateZone(x, y, z)
                 SetVehicleDoorOpen(garbageVehicle,5,false,false)
             else
                 if not Config.UseTarget then
-                    exports['qb-core']:HideText()
+                    exports['qbx-core']:HideText()
                     listen = false
                 end
                 SetVehicleDoorShut(garbageVehicle, 5, false)
@@ -411,11 +411,11 @@ local function spawnPeds()
                 zone:onPlayerInOut(function(inside)
                     if LocalPlayer.state.isLoggedIn then
                         if inside then
-                            exports['qb-core']:DrawText(Lang:t("info.talk"), 'left')
+                            exports['qbx-core']:DrawText(Lang:t("info.talk"), 'left')
                             Listen4Control()
                         else
                             ControlListen = false
-                            exports['qb-core']:HideText()
+                            exports['qbx-core']:HideText()
                         end
                     end
                 end)
--- server/main.lua
+++ server/main.lua
@@ -1,4 +1,4 @@
-local QBCore = exports['qb-core']:GetCoreObject()
+local QBCore = exports['qbx-core']:GetCoreObject()
 local Routes = {}
 
 local function CanPay(Player)
