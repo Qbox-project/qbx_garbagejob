8a848e5fef38b33cdbe5840e9b677071ab4b4db1
DevX32 <dishantacnc389@gmail.com>
2023-11-08 14:19:27 +0545

    correct progressbar

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,8 +5,8 @@ description 'qbx_garbagejob'
 version '1.0.0'
 
 shared_scripts {
-        '@ox_lib/init.lua',
-        '@qbx_core/import.lua',
+    '@ox_lib/init.lua',
+    '@qbx_core/import.lua',
 	'@qbx_core/shared/locale.lua',
 	'locales/en.lua',
 	'locales/*.lua',
@@ -14,11 +14,6 @@ shared_scripts {
 }
 
 client_script {
-	'@PolyZone/client.lua',
-	'@PolyZone/BoxZone.lua',
-	'@PolyZone/EntityZone.lua',
-	'@PolyZone/CircleZone.lua',
-	'@PolyZone/ComboZone.lua',
 	'client/main.lua'
 }
 server_script 'server/main.lua'
