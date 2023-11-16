206adbf969f0a69cdeca430ad317ab1111d0fd67
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-11-16 13:53:01 -0500

    fix: import modules
    * fix: import modules
    
    * fix: missing letter
    
    * fix: spacing

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,19 +1,21 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'qbx_garbagejob'
+description 'QBX_GarbageJob'
+repository 'https://github.com/Qbox-project/qbx_garbagejob'
 version '1.0.0'
 
 shared_scripts {
-        '@ox_lib/init.lua',
-        '@qbx_core/import.lua',
+	'@ox_lib/init.lua',
+	'@qbx_core/modules/utils.lua',
 	'@qbx_core/shared/locale.lua',
 	'locales/en.lua',
 	'locales/*.lua',
 	'config.lua'
 }
 
-client_script {
+client_scripts {
+	'@qbx_core/modules/playerdata.lua',
 	'@PolyZone/client.lua',
 	'@PolyZone/BoxZone.lua',
 	'@PolyZone/EntityZone.lua',
@@ -21,12 +23,8 @@ client_script {
 	'@PolyZone/ComboZone.lua',
 	'client/main.lua'
 }
-server_script 'server/main.lua'
 
-modules {
-    'qbx_core:utils',
-    'qbx_core:playerdata',
-}
+server_script 'server/main.lua'
 
 lua54 'yes'
 use_experimental_fxv2_oal 'yes'
